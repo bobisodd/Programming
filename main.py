452cf5695ccad25e50397fc300e4a7a9b88d3319
bobisodd <89556856+bobisodd@users.noreply.github.com>
2021-09-14 08:19:39 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# x = int(input("Please enter your first number: "))
+# y = int(input("Second number please: "))
+# print(x + y)
+
+height = int(input('Enter your height in inches: '))
+weight = int(input('Enter your weight in lbs.: '))
+
+bmi = (weight/(height**2))*703
+print('Your BMI is: ' + str(bmi))
+
+if(bmi < 20):
+    print("Awesome.")
+elif(bmi < 30):
+    print("Need some working out there.")
+elif(bmi < 40):
+    print("Uh Oh... go see a doctor.")
+else:
+    print("Seriously comma need some help. You, might, be, in, a , commmmmmmmma, soon.")
